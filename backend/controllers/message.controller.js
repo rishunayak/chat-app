273a36c1ebaee7e3b8e631f9ac649f3f5f5ee82b
rishunayak <rishunayak22@gmail.com>
2024-02-19 13:51:08 +0530

    Completed

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage=async(req,res)=>
 {
@@ -32,12 +33,14 @@ export const sendMessage=async(req,res)=>
             conversation.message.push(newMessage._id);
         }
 
-        // await conversation.save();
-        // await newMessage.save();
-
-        // Socket.io
 
         await Promise.all([conversation.save(),newMessage.save()])
+
+        const receiverSocketId=getReceiverSocketId(receiverId)
+        if(receiverSocketId)
+        {
+            io.to(receiverSocketId).emit("newMessage",newMessage)
+        }
         res.status(201).json(newMessage)
     } catch (error) {
         console.log("Error in sendMessage Controller",error.message)
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser"
@@ -7,8 +8,10 @@ import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js"
+import { app, server } from "./socket/socket.js";
 
-const app=express();
+
+const __dirname=path.resolve();
 
 dotenv.config();
 
@@ -19,7 +22,12 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
 
+app.get("*",(req,res)=>
+{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 // app.get("/",(req,res)=>
 // {
@@ -29,7 +37,7 @@ app.use("/api/users",userRoutes);
 
 
 
-app.listen(8080,()=>
+server.listen(8080,()=>
 {
     connectToMongoDB();
     console.log("server is running on port 8080");
